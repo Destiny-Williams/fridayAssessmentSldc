e59b10999294527677b9b8b4ffbbd7ef8ff66f60
Destiny Williams <92333772+Destiny-Williams@users.noreply.github.com>
2021-10-15 17:28:33 -0500

    Adding sldc file with details
    Committing my changes to the sldc file.

---
+++ sldc.js
@@ -0,0 +1,28 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+// 
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle 
+    and 
+    set the values of each variable to a definition of the step.
+*/
+
+const planning = ("Come up with a plan for your product with your team.")
+const analysisOfRequirments = ("Working with your team to figure out what is needed for project to be successful. Whos and what is needed.")
+const design = ("Create models of the software application, while figuring out what your design will need to inclued.")
+const implementation = ("Creation of a processes and procedures needed to take sofware applications from the Planning stage to a live and active programed software.")
+const testingAndIntegration = ("Testing ensures that each function works correctly.")
+const maintain = ("Maintenance is to make sure software continues to work and remains bug free.")
